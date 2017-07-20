9769f95461c81645a3e619d4327e43df8932f5ae
Belyj <Belyj_07@inbox.ru>
2017-07-21 01:08:12 +0400

    Удалил пустые файлы.

--- src/main/java/ru/handbook/serialization/SerialDAO.java
+++
@@ -1,7 +0,0 @@
-package ru.handbook.serialization;
-
-/**
- * Created by operator1 on 19.07.2017.
- */
-public abstract class SerialDAO  {
-}
--- src/main/java/ru/handbook/serialization/SerialDAOFactory.java
+++
@@ -1,8 +0,0 @@
-package ru.handbook.serialization;
-
-/**
- * Created by operator1 on 19.07.2017.
- */
-public class SerialDAOFactory  {
-    
-}
